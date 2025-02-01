278c45fa0680142b53fc6cb39728d65b98c6905d
alexbaumgertner <alex.baumgertner@gmail.com>
2025-02-02 01:18:46 +0300

    Show errors and dont use polling

--- src/app/api/telegram/route.ts
+++ src/app/api/telegram/route.ts
@@ -2,22 +2,27 @@ import { NextResponse } from 'next/server'
 import TelegramBot from 'node-telegram-bot-api'
 
 const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN!
-const bot = new TelegramBot(TELEGRAM_TOKEN)
+const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: false })
 
 export async function POST(request: Request) {
-  const body = await request.json()
+  try {
+    const body = await request.json()
 
-  if (body.message) {
-    const chatId = body.message.chat.id
-    const text = body.message.text
+    if (body.message) {
+      const chatId = body.message.chat.id
+      const text = body.message.text
 
-    console.log({
-      chatId,
-      text,
-    })
+      console.log({
+        chatId,
+        text,
+      })
 
-    await bot.sendMessage(chatId, `You said (test for Niko): ${text}`)
+      await bot.sendMessage(chatId, `You said (test for Niko): ${text}`)
+    }
+
+    return NextResponse.json({ status: 'ok' })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ status: 'error' }, { status: 500 })
   }
-
-  return NextResponse.json({ status: 'ok' })
 }
